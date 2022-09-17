4ea93ebd835f19490698d9e17ef05402962b4a35
Kaneria Krunal <prokillerkk@outlook.com>
2022-09-18 01:31:59 +0530

    Introduce root saga and create category saga

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,14 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import {persistReducer,persistStore} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from 'redux-logger';
-import thunk from "redux-thunk";
+// import thunk from "redux-thunk";
+import createSagaMiddleware from "@redux-saga/core";
+import { rootSaga } from "./root-saga";
 
 import { rootReducer } from './root-reducer';
 import { configureStore } from "@reduxjs/toolkit";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const loggerMiddleware = (store) => (next) => (action) => {
   if (!action.type) {
@@ -34,6 +38,8 @@ const persistedReducer = persistReducer(persistConfig,rootReducer);
 // const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 // export const store = createStore(persistedReducer, undefined, composedEnhancers);
-export const store = configureStore({reducer:persistedReducer,middleware:[thunk,logger]});
+export const store = configureStore({reducer:persistedReducer,middleware:[sagaMiddleware,logger]});
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store); 
